5dde2c342b1b0d847e939167b1b7315d407cd791
= <=>
2018-03-27 00:44:46 +0800

    .

---
+++ novelreader/novelreader/views/chapter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/evn python
+
+import logging
+from django.http import Http404
+from django.views.generic import TemplateView
+from ..models.novelutil import (
+    get_novel_info, get_latest_chapters, get_chapters,
+    list_hot_novels, list_recommend_novels
+)
+from .base import BaseViewMixin
+
+__author__ = 'samuel'
+
+log = logging.getLogger(__name__)
+
+
+class ChapterView(TemplateView, BaseViewMixin):
+
+    def get(self, request, *args, **kwargs):
+        return super(ChapterView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ChapterView, self).get_context_data(**kwargs)
+        nid = kwargs.get('nid', None)
+        if nid is None:
+            raise Http404('Novel does not exist.')
+        context['novel'] = get_novel_info(nid, add_last_chapter=True)
+        context['latest_chapters'] = get_latest_chapters(nid)
+        context['chapters'] = get_chapters(nid)
+        context['hot_novels'] = list_hot_novels(page_items=6)
+        context['recommend_novels'] = list_recommend_novels(page_items=5)
+
+        return context
