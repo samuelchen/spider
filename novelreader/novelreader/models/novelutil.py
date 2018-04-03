24edcc8fe963cc6c8d7bd3290e19b0319e939d5a
Samuel Chen <samuel.net@gmail.com>
2018-04-04 00:07:40 +0800

    '.'

--- novelreader/novelreader/models/novelutil.py
+++ novelreader/novelreader/models/novelutil.py
@@ -194,7 +194,7 @@ novels_has_chapters = list_novels_has_chapters()
 
 def _get_category_where_clause(category):
     tn = db.DB_table_novel
-    if category == '完本':
+    if category == '全本':
         where_clause = and_(tn.c.status=='已完本')
     else:
         where_clause = and_(tn.c.category==category)
