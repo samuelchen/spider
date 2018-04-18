2c9828f12d9daff413b06a6e1f860473ea1895bb
Samuel Chen <samuel.net@gmail.com>
2018-04-18 17:12:45 +0800

    .

--- syncmgr/azuremgr.py
+++ syncmgr/azuremgr.py
@@ -9,7 +9,7 @@ from conf import AzureConf
 
 class AzureSyncManager(object):
 
-    blob_bucket = 'https://novels.blob.core.windows.net/reader/'
+    # blob_bucket = 'https://novels.blob.core.windows.net/reader/'
     container = 'reader'
 
     def __init__(self, acc_name, acc_key, blob_container, local_static_root):
@@ -44,58 +44,63 @@ class AzureSyncManager(object):
             except Exception as err:
                 print(err)
 #
-# def run_sample():
-#     try:
-#         # Create the BlockBlockService that is used to call the Blob service for the storage account
-#         block_blob_service = BlockBlobService(account_name='accountname', account_key='accountkey')
-#
-#         # Create a container called 'quickstartblobs'.
-#         container_name ='quickstartblobs'
-#         block_blob_service.create_container(container_name)
-#
-#         # Set the permission so the blobs are public.
-#         block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
-#
-#         # Create a file in Documents to test the upload and download.
-#         local_path=os.path.expanduser("~/Documents")
-#         local_file_name ="QuickStart_" + str(uuid.uuid4()) + ".txt"
-#         full_path_to_file =os.path.join(local_path, local_file_name)
-#
-#         # Write text to the file.
-#         file = open(full_path_to_file,  'w')
-#         file.write("Hello, World!")
-#         file.close()
-#
-#         print("Temp file = " + full_path_to_file)
-#         print("\nUploading to Blob storage as blob" + local_file_name)
-#
-#         # Upload the created file, use local_file_name for the blob name
-#         block_blob_service.create_blob_from_path(container_name, local_file_name, full_path_to_file)
-#
-#         # List the blobs in the container
-#         print("\nList blobs in the container")
-#         generator = block_blob_service.list_blobs(container_name)
-#         for blob in generator:
-#             print("\t Blob name: " + blob.name)
-#
-#         # Download the blob(s).
-#         # Add '_DOWNLOADED' as prefix to '.txt' so you can see both files in Documents.
-#         full_path_to_file2 = os.path.join(local_path, str.replace(local_file_name ,'.txt', '_DOWNLOADED.txt'))
-#         print("\nDownloading blob to " + full_path_to_file2)
-#         block_blob_service.get_blob_to_path(container_name, local_file_name, full_path_to_file2)
-#
-#         sys.stdout.write("Sample finished running. When you hit <any key>, the sample will be deleted and the sample "
-#                          "application will exit.")
-#         sys.stdout.flush()
-#         input()
-#
-#         # Clean up resources. This includes the container and the temp files
-#         block_blob_service.delete_container(container_name)
-#         os.remove(full_path_to_file)
-#         os.remove(full_path_to_file2)
-#     except Exception as e:
-#         print(e)
 
+    def sync_folder(self, folder, del_remote=False, del_local=False):
+        root_path = os.path.join(self.local_static_root, folder)
+        print('Local folder path: ' + root_path)
+
+        prefix = ''
+        local_files = set()
+        for root, dirs, files in os.walk(root_path, followlinks=True):
+            print('-' * 70)
+            head = root
+            prefix = ''
+            while head != self.local_static_root:
+                head, tail = os.path.split(head)
+                prefix = '/'.join([tail, prefix])
+            files = set(prefix + name for name in files)
+            print(prefix)
+            print('files:', files)
+            local_files.update(files)
+
+        print('='*70)
+        remote_blobs = self.block_blob_service.list_blobs(self.blob_container, prefix=folder)
+        remote_files = set(blob.name for blob in remote_blobs)
+        print('local files:', local_files)
+        print('remote blobs: ', remote_files)
+
+
+        print('='*70)
+        local_extra = local_files - remote_files
+        remote_extra = remote_files - local_files
+        # print('new files:', new_files)
+        # print('del files:', del_files)
+
+        for fname in local_extra:
+            local_path = os.path.join(self.local_static_root, fname)
+            remote_path = fname
+            try:
+                if del_local:
+                    print(' ^^ \tDEL\t', local_path, ' <-- ', '(remote)')
+                    os.remove(local_path)
+                else:
+                    print(' ^^ \tUPLOAD\t', local_path, ' --> ', remote_path)
+                    self.block_blob_service.create_blob_from_path(self.blob_container, remote_path, local_path)
+            except Exception as err:
+                print(err)
+
+        for fname in remote_extra:
+            local_path = os.path.join(self.local_static_root, fname)
+            remote_path = fname
+            try:
+                if del_remote:
+                    print(' xx \tDEL\t', '(local)', ' --> ', remote_path)
+                    self.block_blob_service.delete_blob(self.blob_container, remote_path)
+                else:
+                    print(' ^^ \tDOWNLOAD\t', local_path, ' <-- ', remote_path)
+                    self.block_blob_service.get_blob_to_path(self.blob_container, remote_path, local_path)
+            except Exception as err:
+                print(err)
 
 # Main method.
 if __name__ == '__main__':
@@ -103,4 +108,5 @@ if __name__ == '__main__':
     azure_syncmgr = AzureSyncManager(AzureConf.ACCOUNT_NAME, AzureConf.ACCOUNT_KEY,
                                      AzureConf.BLOB_CONTAINER, AzureConf.LOCAL_STATIC_ROOT)
 
-    azure_syncmgr.sync_albums()
+    # azure_syncmgr.sync_albums()
+    azure_syncmgr.sync_folder('font-awesome', del_remote=True, del_local=True)
