10f3748665bee5aa873e7636c0e11b5b76ed2346
Samuel Chen <samuel.net@gmail.com>
2018-04-20 14:54:58 +0800

    '.'

--- novelspider/novel.sh
+++ novelspider/novel.sh
@@ -6,6 +6,6 @@ echo "BASE_PATH is ${BASE_PATH}."
 cd ${BASE_PATH}
 #echo ">> ${PWD}"
 mkdir -p log
-nohup scrapy crawl novel -s JOBDIR=./.scrapy-stat/novel-1 > log/novel.log &
+nohup scrapy crawl novel > log/novel.log &
 cd -
 #echo "<< ${PWD}"
